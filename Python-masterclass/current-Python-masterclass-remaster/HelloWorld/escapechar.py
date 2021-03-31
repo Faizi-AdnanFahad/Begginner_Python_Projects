460b074c413a31e064f6398510b426cd4795f7bd
adnan500 <adnan500@my.yorku.ca>
2021-03-30 23:49:29 -0400

    Update

--- Python-masterclass/current-Python-masterclass-remaster/HelloWorld/escapechar.py
+++ Python-masterclass/current-Python-masterclass-remaster/HelloWorld/escapechar.py
@@ -5,4 +5,53 @@ tabbedString = "1\t2\t3\t4\t5"
 print(tabbedString)
 
 print('The pet show owner said "No, no, \'e\'s uh,...he\'s resting"')
+
+# backslash help us to skip a character.
 print("The pet show owner said \"No, no, 'e 's uh,...he's resting\"")
+
+# No need for to skip when triple quote is used
+print("""The pet show owner said "No, no, 'e's uh,...he's resting" """)
+print()
+
+# this will be printed into several lines because of the triple quote
+print("""This string has been 
+split over 
+several 
+lines""")
+
+print()
+
+# these 3 backslash will avoid splitting the lines and will skip the last character at each line which is a hidden *n*
+print("""This string has been \
+split over \
+several \
+lines""")
+
+# Escaping the backslash character
+print("C:\\Users\\timebulchalka\\nores.txt")
+print(r"C:\Users\timebulchalka\nores.txt")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Python-masterclass/current-Python-masterclass-remaster/HelloWorld/strings.py
+++ Python-masterclass/current-Python-masterclass-remaster/HelloWorld/strings.py
@@ -5,8 +5,20 @@ print('We can even include "quote" in strings')
 print("Hello" + " World")
 
 greeting = 'Hello'
-name = input("Please enter your name?")
+name = "Adnan"
 print(greeting + name)
 
 # if we want a space, we can add that too
 print(greeting + " " + name)
+
+age = 24
+print(age)
+print(type(greeting))
+print(type(age))
+
+age_in_words = "24 years"
+
+# unlike Java in Python it's not possible to concatenate integers and Strings.
+# print(age + age_in_words)
+print(age)
+print(type(age))
