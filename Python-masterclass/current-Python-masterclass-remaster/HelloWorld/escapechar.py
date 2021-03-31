640d84b6b88e1223271de6b2f825164b0c35214a
adnan500 <adnan500@my.yorku.ca>
2021-03-30 23:21:06 -0400

    Update

---
+++ Python-masterclass/current-Python-masterclass-remaster/HelloWorld/escapechar.py
@@ -0,0 +1,8 @@
+splitString = "this staring has been \nsplit over\nseveral\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet show owner said "No, no, \'e\'s uh,...he\'s resting"')
+print("The pet show owner said \"No, no, 'e 's uh,...he's resting\"")
